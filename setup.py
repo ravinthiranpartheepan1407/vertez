bacf43170f073e56b107ead8d06c93c7dba9d8ee
surenravi1407 <surenravi1407@gmail.com>
2023-07-02 14:14:08 +0300

    Description

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ ext_modules = [
 
 setup(
     name="vertez",
-    author="Ravinthiran Partheepan"
+    author="Ravinthiran Partheepan",
     description="Vertez: Statify and Infer",
     long_description='''Vertez is a cutting-edge open-source framework company dedicated to developing powerful and versatile libraries for statistical analysis, machine learning, and deep learning. We empower data scientists, researchers, and developers with a comprehensive suite of tools to drive insights and innovations in diverse domains. At Vertez, we believe in democratizing access to advanced statistical techniques and the latest advancements in artificial intelligence. Our libraries provide an intuitive and flexible platform for statistical modeling, predictive analytics, and complex data-driven decision making. With a focus on simplicity, performance, and scalability, we enable practitioners to efficiently explore, analyze, and infer from their data.'''
     version="1.0",
